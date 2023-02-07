7f18de044bf3cccb68656b6cec28a76c5e30743b
jibril101 <mohsinak49@gmail.com>
2023-02-06 22:52:45 -0800

    hardcode gateway api

--- FrontendApplication/src/script.js
+++ FrontendApplication/src/script.js
@@ -1,4 +1,4 @@
-var api = process.env.API_GATEWAY // get the API Gateway from the environment
+var api = "gateway.default" // get the API Gateway from the environment
 
 $(document).ready(function() {
     $("#btn").click(function() {
